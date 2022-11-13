581c5d4e95f78b741deb3011d3a8eb0584bac612
RossSihovsk <rsihovskiy@gmail.com>
2022-11-13 15:39:52 +0200

    Add details activity and overview fragment

--- app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
@@ -1,21 +1,41 @@
 package com.example.foody.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.foody.R
+import com.example.foody.models.Result
+import com.example.foody.screen.RecipesFragmentDirections
+import org.jsoup.Jsoup
 
 class RecipesRowBinding {
 
     companion object {
 
+        @BindingAdapter("onRecipesClickListener")
+        @JvmStatic
+        fun onRecipesClickListener(recipesRowLayout: ConstraintLayout, result: Result) {
+            recipesRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipesRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipesClickListener", e.toString())
+                }
+            }
+        }
+
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
-        fun loadImageFromUrl(imageView: ImageView, url:String){
-            imageView.load(url){
+        fun loadImageFromUrl(imageView: ImageView, url: String) {
+            imageView.load(url) {
                 crossfade(600)
                 error(R.drawable.ic_error_placeholder)
             }
@@ -55,6 +75,15 @@ class RecipesRowBinding {
                         )
                     }
                 }
+            }
+        }
+
+        @BindingAdapter("parseHtml")
+        @JvmStatic
+        fun parseHtml(textView: TextView, description: String?) {
+            if (description != null) {
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
             }
         }
     }
